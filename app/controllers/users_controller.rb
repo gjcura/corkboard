ac416fc985e20790e2ea18ea1d70d827d6abc0da
Garrett <garrettcura@gmail.com>
2013-11-17 12:44:27 -0500

    total new layout still needs a lot of work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ before_action :admin_user,     only: :destroy
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to NCSU Corkboard!"
       redirect_to @user
     else
       render 'new'
